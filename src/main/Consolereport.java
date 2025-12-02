10f4ac847ee31995042bc995b458238741ce876a
SilverWolfiee <rblenderdrag@gmail.com>
2025-12-02 10:52:59 +0700

    Add proper Guest checking

--- src/main/Consolereport.java
+++ src/main/Consolereport.java
@@ -5,6 +5,11 @@ public class Consolereport implements Report {
 	@Override
 	public void print(String name, String gpa) {
 		// TODO Auto-generated method stub
+		if (gpa.equals("N/A")) {
+	        System.out.println("Guest student detected — console export skipped.\n");
+	        System.out.println("Guest : "+name);
+	        return;
+	    }
 		System.out.println("=====Report======");
 		System.out.println("=================");
 		System.out.println("Student : "+name);
--- src/main/Excelreport.java
+++ src/main/Excelreport.java
@@ -4,14 +4,19 @@ public class Excelreport implements Report{
 
 	@Override
 	public void print(String name, String gpa) {
-		// TODO Auto-generated method stub
-		System.out.println("Output has been exported to"+name+".xlsx");
-		System.out.println("=====Report======");
-		System.out.println("=================");
-		System.out.println("Student : "+name);
-		System.out.println("GPA : "+gpa);
-		System.out.println("================");
-		
+	    if (gpa.equals("N/A")) {
+	        System.out.println("Guest student detected — Excel export skipped.\n");
+	        System.out.println("Guest : "+name);
+	        return;
+	    }
+
+	    System.out.println("Output has been exported to " + name + ".xlsx");
+	    System.out.println("=====Report======");
+	    System.out.println("=================");
+	    System.out.println("Student : " + name);
+	    System.out.println("GPA : " + gpa);
+	    System.out.println("================");
 	}
+
 
 }
